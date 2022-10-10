2a1a1168ad450c5dd55a4b2a413cfd160dff3282
Katarzyna Maciejewska <katarzyna.maciejewska@swmansion.com>
2022-10-10 10:23:55 +0200

    add getPokemons with names and images

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,26 @@
-const test = 'test';
 
-console.log(test);
+
+async function getPokemons() {
+    for (let id = 1; id < 21; id++) {
+        let pokeapi = `https://pokeapi.co/api/v2/pokemon/${id}`
+        const response = await fetch(pokeapi);
+        const data = await response.json();
+        const pokeName = data.name
+        const pokeImgUrl = data.sprites.other['official-artwork'].front_default
+
+        const all = document.getElementById('all')
+        const pokeDiv = document.createElement('div')
+        const pokeP = document.createElement('p')
+        pokeP.textContent = pokeName.charAt(0).toUpperCase() + pokeName.slice(1)
+        const pokeImg = document.createElement('img')
+        pokeImg.src = pokeImgUrl
+
+        pokeDiv.appendChild(pokeP)
+        pokeDiv.appendChild(pokeImg)
+
+        all.appendChild(pokeDiv)
+    }
+}
+
+
+getPokemons()
