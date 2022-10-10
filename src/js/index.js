0aeeb85fd440d7786cba5e99c91c8de8356e62b0
Katarzyna Maciejewska <katarzyna.maciejewska@swmansion.com>
2022-10-10 14:41:20 +0200

    add load more pokemons button

--- src/js/index.js
+++ src/js/index.js
@@ -1,28 +1,54 @@
+const pokeList = document.getElementById('pokeList')
+const getMorePokeBtn = document.getElementById('getMorePokemons')
+
+async function getPokeInfo(poke) {
+    let url = poke.url
+    const response = await fetch(url);
+    const data = await response.json();
+}
+
+let offset = 0
+
+function loadMore() {
+    offset = offset + 20
+    getPokemons()
+}
+
+getMorePokeBtn.addEventListener('click', loadMore)
 
 
 async function getPokemons() {
-    for (let id = 1; id < 21; id++) {
-        let pokeapi = `https://pokeapi.co/api/v2/pokemon/${id}`
-        const response = await fetch(pokeapi);
-        const data = await response.json();
-        const pokeName = data.name
-        const pokeImgUrl = data.sprites.other['official-artwork'].front_default
+    let pokeapi = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=20`
+    const response = await fetch(pokeapi);
+    const data = await response.json();
+    const pokemonsArr = data.results
 
-        const pokeList = document.getElementById('pokeList')
-        const pokeDiv = document.createElement('div')
-        pokeDiv.classList.add('pokeWrapper')
-        const pokeP = document.createElement('p')
-        pokeP.textContent = pokeName.charAt(0).toUpperCase() + pokeName.slice(1)
-        const pokeImg = document.createElement('img')
-        pokeImg.src = pokeImgUrl
+    pokemonsArr.map(pokemon => getPokeInfo(pokemon))
 
-        pokeDiv.appendChild(pokeImg)
-        pokeDiv.appendChild(pokeP)
+    // const pokeName = data.name
+    // const pokeImgUrl = data.sprites.other['official-artwork'].front_default
 
+    // const handleGetPokeInfo = () => {
+    //     console.log(data)
+    // }
 
-        pokeList.appendChild(pokeDiv)
-    }
+    // const pokeDiv = document.createElement('div')
+    // pokeDiv.addEventListener('click', handleGetPokeInfo)
+    // pokeDiv.classList.add('pokeWrapper')
+    // const pokeP = document.createElement('p')
+    // pokeP.classList.add('pokeName')
+    // pokeP.textContent = pokeName.charAt(0).toUpperCase() + pokeName.slice(1)
+    // const pokeImg = document.createElement('img')
+    // pokeImg.src = pokeImgUrl
+
+    // pokeDiv.appendChild(pokeImg)
+    // pokeDiv.appendChild(pokeP)
+
+    // pokeList.appendChild(pokeDiv)
+    // getMorePokeBtn.style.display = 'block'
 }
 
-
 getPokemons()
+
+
+
