f671c8335cb35906e30a8999d010ca92a76cdfb9
Katarzyna Maciejewska <katarzyna.maciejewska@swmansion.com>
2022-10-11 13:21:08 +0200

    fix fetching pokemon

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,14 @@
 const pokeList = document.getElementById('pokeList')
 const getMorePokeBtn = document.getElementById('getMorePokemons')
 
+
 async function getPokeInfo(poke) {
     let url = poke.url
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data)
+    pokemonsDetails = []
+    pokemonsDetails.push(data)
+    return pokemonsDetails
 }
 
 let offset = 0
@@ -14,6 +17,7 @@ function loadMore() {
     offset = offset + 20
     getPokemons()
     getDetails()
+    renderPokemons()
 }
 
 getMorePokeBtn.addEventListener('click', loadMore)
@@ -24,45 +28,47 @@ async function getPokemons() {
     const response = await fetch(pokeapi);
     const data = await response.json();
     let pokemonsArr = data.results
-
     return pokemonsArr
-
-    // const pokeName = data.name
-    // const pokeImgUrl = data.sprites.other['official-artwork'].front_default
-
-    // const handleGetPokeInfo = () => {
-    //     console.log(data)
-    // }
-
-    // const pokeDiv = document.createElement('div')
-    // pokeDiv.addEventListener('click', handleGetPokeInfo)
-    // pokeDiv.classList.add('pokeWrapper')
-    // const pokeP = document.createElement('p')
-    // pokeP.classList.add('pokeName')
-    // pokeP.textContent = pokeName.charAt(0).toUpperCase() + pokeName.slice(1)
-    // const pokeImg = document.createElement('img')
-    // pokeImg.src = pokeImgUrl
-
-    // pokeDiv.appendChild(pokeImg)
-    // pokeDiv.appendChild(pokeP)
-
-    // pokeList.appendChild(pokeDiv)
-    // getMorePokeBtn.style.display = 'block'
 }
 
 getPokemons()
 
-// const getDetails = () => {
-//     pokemonsArr.map(pokemon => getPokeInfo(pokemon))
-// }
-
-
-
-const getDetails = () => {
-    let pokemons = getPokemons()
-    pokemons.then(result => console.log(result))
+const getDetails = async function () {
+    let pokemons = await getPokemons()
+    let pokeDetails = await pokemons.map(pokemon => getPokeInfo(pokemon))
+    return pokeDetails
 }
 
-getDetails()
+const resolvePromisesSeq = async (tasks) => {
+    const results = [];
+    for (const task of tasks) {
+        results.push(await task);
+    }
+    return results;
+};
 
+const renderPokemons = async function () {
+    let pokeDetailsPromise = await getDetails()
+    let pokeDetailsArr = await resolvePromisesSeq(pokeDetailsPromise)
+    let pokeDetailsToRender = pokeDetailsArr.flat()
 
+    pokeDetailsToRender.map(pokemon => {
+        const pokeName = pokemon.name
+        const pokeImgUrl = pokemon.sprites.other['official-artwork'].front_default
+        const pokeDiv = document.createElement('div')
+        pokeDiv.classList.add('pokeWrapper')
+        const pokeP = document.createElement('p')
+        pokeP.classList.add('pokeName')
+        pokeP.textContent = pokeName.charAt(0).toUpperCase() + pokeName.slice(1)
+        const pokeImg = document.createElement('img')
+        pokeImg.src = pokeImgUrl
+
+        pokeDiv.appendChild(pokeImg)
+        pokeDiv.appendChild(pokeP)
+
+        pokeList.appendChild(pokeDiv)
+        getMorePokeBtn.style.display = 'block'
+    })
+}
+
+renderPokemons()
