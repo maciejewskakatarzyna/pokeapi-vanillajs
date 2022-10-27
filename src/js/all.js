908d144c040b4c978b9fe23f47bb53357d27357a
Katarzyna Maciejewska <katarzyna.maciejewska@swmansion.com>
2022-10-27 10:53:03 +0200

    add hover to poke wrapper and heart icon

--- src/js/all.js
+++ src/js/all.js
@@ -2,6 +2,7 @@ import { resolvePromisesSeq } from "./utils/resolvePromisesSeq.js"
 import { handleInfiniteScroll } from "./utils/infiniteScroll.js"
 import { removeAllChildNodes } from "./utils/removeAllChildNodes.js"
 import { setOffset } from "./utils/setOffset.js"
+import { html, render } from 'https://unpkg.com/lit-html@1.3.0/lit-html.js'
 
 const pokeList = document.querySelector('.pokeList')
 const getMorePokeBtn = document.querySelector('.getMorePokemons')
@@ -10,7 +11,6 @@ const noDetailsInfo = document.querySelector('.noDetailsInfo')
 
 let offset = 0
 
-//window.onLoad czy DOMContentLoaded?
 document.addEventListener('DOMContentLoaded', getPokemons)
 document.addEventListener('DOMContentLoaded', renderPokemons)
 
@@ -26,15 +26,20 @@ async function getPokemons() {
     return pokemonsArr
 }
 
-//resolvePromises ???
 async function renderPokemons() {
     const pokeDetailsPromise = await getDetails()
     const pokeDetailsArr = await resolvePromisesSeq(pokeDetailsPromise)
     const pokeDetailsToRender = pokeDetailsArr.flat()
 
-    //dlaczego nie działa zmiana image.src?
-    const handlePokeLike = ((image, pokemon) => {
-        ((image.src === "./dist/assets/heart.svg") ? (image.src = "./dist/assets/heart2.svg") : (image.src = "./dist/assets/heart.svg"))
+    const handlePokeLike = ((button, pokemon) => {
+
+
+        if (button.dataset.active != 'true') {
+            button.dataset.active = 'true';
+        } else {
+            button.dataset.active = "false"
+        }
+
 
         localStorage.setItem("favourite_pokemon", JSON.stringify(pokemon))
     })
@@ -51,13 +56,28 @@ async function renderPokemons() {
         pokeImg.src = pokeImgUrl
         const heart = document.createElement('button')
         heart.classList.add('heartBtn')
-        const heartIcon = document.createElement('img')
-        heartIcon.classList.add('heartIcon')
-        heartIcon.src = "./dist/assets/heart.svg"
 
-        heart.addEventListener('click', () => handlePokeLike(heartIcon, pokemon))
+        const icon = html`
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+    <path opacity="0.15"
+        d="M4.3314 12.0474L12 20L19.6686 12.0474C20.5211 11.1633 21 9.96429 21 8.71405C21 6.11055 18.9648 4 16.4543 4C15.2487 4 14.0925 4.49666 13.24 5.38071L12 6.66667L10.76 5.38071C9.90749 4.49666 8.75128 4 7.54569 4C5.03517 4 3 6.11055 3 8.71405C3 9.96429 3.47892 11.1633 4.3314 12.0474Z"
+        fill="none" />
+    <path
+        d="M4.3314 12.0474L12 20L19.6686 12.0474C20.5211 11.1633 21 9.96429 21 8.71405C21 6.11055 18.9648 4 16.4543 4C15.2487 4 14.0925 4.49666 13.24 5.38071L12 6.66667L10.76 5.38071C9.90749 4.49666 8.75128 4 7.54569 4C5.03517 4 3 6.11055 3 8.71405C3 9.96429 3.47892 11.1633 4.3314 12.0474Z"
+        stroke="gray" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+</svg>
+        `
 
-        heart.appendChild(heartIcon)
+        render(icon, heart)
+
+        console.log(icon);
+        // const heartIcon = document.createElement('img')
+        // heartIcon.classList.add('heartIcon')
+        // heartIcon.src = "./dist/assets/heart.svg"
+
+        heart.addEventListener('click', () => handlePokeLike(heart, pokemon))
+
+        // heart.appendChild(heartIcon)
 
         pokeDiv.appendChild(pokeImg)
         pokeDiv.appendChild(pokeP)
--- src/js/utils/setOffset.js
+++ src/js/utils/setOffset.js
@@ -1,5 +1,5 @@
 export const setOffset = (el) => {
-    var rect = el.getBoundingClientRect(),
+    let rect = el.getBoundingClientRect(),
         scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
         scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
